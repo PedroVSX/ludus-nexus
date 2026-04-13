8e260ab721e019a013aab6abf6a4bd6c6166e7c0
PedroVSX <pedrovsx2004@gmail.com>
2026-04-13 13:10:00 -0300

    Páginas de login e cadastro criadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { SignUp } from './pages/sign-up/sign-up';
+import { SignIn } from './pages/sign-in/sign-in';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'sign-up', pathMatch: 'full' },
-    { path: 'sign-up', component: SignUp }
+    { path: 'sign-up', component: SignUp },
+    { path: 'sign-in', component: SignIn }
 ];
